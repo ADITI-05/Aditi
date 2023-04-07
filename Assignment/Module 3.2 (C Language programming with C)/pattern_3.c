9f7e95aab16ce5334a2d51408e7f72cec48da73e
ADITI-05 <aditideshani2001@gmail.com>
2023-04-07 12:59:24 +0530

    complete

---
+++ Assignment/Module 3.2 (C Language programming with C)/pattern_3.c
@@ -0,0 +1,21 @@
+/* Pattern 3 : 
+*/
+
+#include<stdio.h>
+main()
+{
+	int i, space, rows, k=0;
+	for(i=1;i<=5;i++,k=0)
+	{
+		for(space=1;space<=rows-i;space++)
+		{
+			printf(" ");
+		}
+		while(k != 2 * i-1 )
+		{
+			printf("*");
+			k++;
+		}
+		printf("\n");
+	}
+}

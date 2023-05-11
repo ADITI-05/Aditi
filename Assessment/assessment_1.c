b3bbdf2ee92c5dd0a5e2644ab1e9faba550465fe
ADITI-05 <aditideshani2001@gmail.com>
2023-05-11 13:51:04 +0530

    complete

--- Assessment/assessment_1.c
+++ Assessment/assessment_1.c
@@ -1,5 +1,18 @@
+/*Create a Food billing system
+
+Make sure each business logic is denoted with appropriate comments and make your code interactive and represent clean and clear output on your console screen.
+
+Adhere to the coding principles
+
+Firstly, display the food items available
+Then after the user can choose any of the item displayed Also take the quantity of selected food item by the customer, then ask the user that he/she wanna select
+more? If yes then again display the food items available and take an order from the customer. Here, you have to consider the total bill as the price of food items
+previously selected plus the price of new items added should display as a whole bill.
+If no then display the final bill on the screen
+*/
+
 #include<stdio.h>
-main()
+int main()
 {
 	int num,quantity;
 	float pri,amt,total;
@@ -13,43 +26,63 @@ main()
 	printf("3.Dosa        \tPrice = 120rs/pcs \n");
 	printf("4.Idli        \tPrice = 50rs/pcs  \n");
 	
-	printf("Please Enter your choice... :");
+	printf("\nPlease Enter your choice... : ");
 	scanf("%d", &num);
-	printf("Enter the quantity :");
-	scanf("%d", &quantity);
 	
-	switch(num)
+	if(num >= 0)
 	{
-		case 1:
-			pri=180;
-			break;
-		case 2:
-			pri=100;
-			break;
-		case 3:
-			pri=120;
-			break;
-		case 4:
-			pri=50;
-			break;
-		default:
-			printf("\nSorry Unavailable choose...\n");
-			break;
+		if(num == 1){
+			printf("\nYou have selected Pizza.");
+			printf("\n\nEnter the quantity : ");
+			scanf("%d",&quantity);
+			pri = 180;
+			amt = pri * quantity;
+			printf("\nAmount = %.2f",amt);
+			total = total + amt;
+			printf("\nTotal Amount is = %.2f\n",total); 
 		}
-		
-		amt=pri*quantity;
-		printf("Amount = %.2f\n", amt);
-		
-		total=total+amt;
-		printf("total amount is = %.2f\n", total);
-		
-		printf("Do you want to place more orders ? y & n :");
-		scanf("%s", &ch);
-		if(ch == 'n')
+		else if(num == 2){
+		    printf("\nYou have selected Burger.");
+			printf("\n\nEnter the quantity : ");
+			scanf("%d",&quantity);
+			pri = 100;
+			amt = pri * quantity;
+			printf("\nAmount = %.2f",amt);
+			total = total + amt;
+			printf("\nTotal Amount is = %.2f\n",total); 	
+		}
+		else if(num == 3){
+			printf("\nYou have selected Dhosa.");
+			printf("\n\nEnter the quantity : ");
+			scanf("%d",&quantity);
+			pri = 120;
+			amt = pri * quantity;
+			printf("\nAmount = %.2f",amt);
+			total = total + amt;
+			printf("\nTotal Amount is = %.2f\n",total); 
+		}
+		else if(num == 4){
+			printf("\nYou have selected Idli.");
+			printf("\n\nEnter the quantity : ");
+			scanf("%d",&quantity);
+			pri = 50;
+			amt = pri * quantity;
+			printf("\nAmount = %.2f",amt);
+			total = total + amt;
+			printf("\nTotal Amount is = %.2f\n",total); 
+		}
+		else{
+			printf("\nINVALID INPUT!!!!!!");
+		}
+	}
+		printf("\nDo you want to place more orders ? y & n : ");
+		scanf("%s",&ch);
+		if(ch=='n')
 		{
 			return 0;
-		}				
+		}		
 	}
+	return 0;
 }
 	
 	
